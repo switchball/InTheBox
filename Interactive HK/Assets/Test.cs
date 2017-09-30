1584d35755359221b0ac2605e3603d5052a9149f
KurisuL <Kurisu_L@163.com>
2017-10-01 05:12:10 +0800

    [ADD] 1.加入了一个准星 2.*不知道怎么做 4.*暂时未做 5.加入可追随Player旋转并根据距离显示消失的字幕，制作了旋转字幕 8.全黑色界面完成 9.尝试添加了一个天空盒

---
+++ Interactive HK/Assets/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    public float distanceThreshold = 5.0f;
+    public float changeSpeed = 5.0f;
+    public Renderer render;
+    private GameObject player;
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void FixedUpdate () {
+        Vector3 distance = player.transform.position - gameObject.transform.position;
+        Color tempColor = gameObject.GetComponent<Renderer>().material.GetColor("_TintColor");
+        if (distance.x * distance.x + distance.y * distance.y + distance.z * distance.z < distanceThreshold * distanceThreshold) {
+            render.enabled = true;
+            if (tempColor.a < 1) {
+                tempColor.a += changeSpeed * 1 / 255.0f;
+                gameObject.GetComponent<Renderer>().material.SetColor("_TintColor", tempColor);
+            }
+        } else {
+            if(tempColor.a > 0) {
+                tempColor.a -= changeSpeed * 1 / 255.0f;
+                gameObject.GetComponent<Renderer>().material.SetColor("_TintColor", tempColor);
+            } else {
+                render.enabled = false;
+            }
+        }
+    }
+}
