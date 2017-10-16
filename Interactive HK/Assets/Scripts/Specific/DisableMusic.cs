8d887a6fbe4c3541e928d2924093a63e0166feaf
Art9 <xyc5000@hotmail.com>
2017-10-16 12:26:19 +0800

    111

---
+++ Interactive HK/Assets/Scripts/Specific/DisableMusic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableMusic : MonoBehaviour {
+    public GameObject obj;
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(A());
+    }
+
+    IEnumerator A()
+    {
+        yield return new WaitForSeconds(1.0f);
+        obj = GameObject.FindGameObjectWithTag("SingleBGM");
+    }
+
+    public void DisableBGM()
+    {
+        obj.GetComponent<TransitionManager>().TransitionOut();
+        StartCoroutine(DisableGO());
+    }
+
+    IEnumerator DisableGO()
+    {
+        yield return new WaitForSeconds(3.0f);
+        obj.SetActive(false);
+        Destroy(obj);
+    }
+}
